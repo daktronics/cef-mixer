8a02031429442828af61715e7ef9a8766f489cf5
Greg Wessels <greg.wessels@daktronics.com>
2018-11-07 15:42:28 -0600

    Add ability to open multiple windows

--- src/main.cpp
+++ src/main.cpp
@@ -18,14 +18,309 @@ extern "C"
 	__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
 	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
 }
-				
-				
-HWND create_window(HINSTANCE, std::string const& title, int width, int height);
-LRESULT CALLBACK wnd_proc(HWND, UINT, WPARAM, LPARAM);
 
-int sync_interval_ = 1;
-bool resize_ = false;
+class Window;
+
+// globals .. yuck
 bool show_devtools_ = false;
+std::vector<Window*> windows_;
+
+
+class Window
+{
+private:
+	HINSTANCE const instance_;
+	HWND hwnd_;
+	std::shared_ptr<d3d11::Device> const device_;
+	std::shared_ptr<d3d11::SwapChain> swapchain_;
+	std::shared_ptr<Composition> const composition_;
+	int sync_interval_;
+	bool resize_;
+	std::string const json_;
+	
+public:
+
+	Window(
+		HINSTANCE instance,
+		std::shared_ptr<d3d11::Device> const& device, 
+		std::shared_ptr<Composition> const& comp,
+		std::string const& json)
+		: instance_(instance)
+		, device_(device)
+		, composition_(comp) 
+		, sync_interval_(1)
+		, resize_(false)
+		, json_(json)
+	{
+	}
+
+	HWND hwnd() const {
+		return hwnd_;
+	}
+
+	static Window* open(
+		HINSTANCE instance, std::string const& json, int32_t width, int32_t height)
+	{
+		// create a D3D11 rendering device
+		auto device = d3d11::create_device();
+		if (!device) {
+			return nullptr;
+		}
+
+		// create a composition to represent our 2D-scene
+		auto const comp = create_composition(device, json);
+		if (!comp) {
+			return nullptr;
+		}
+
+		LPCWSTR class_name = L"_main_window_";
+
+		WNDCLASSEXW wcex = {};
+		wcex.cbSize = sizeof(WNDCLASSEX);
+		if (!GetClassInfoEx(instance, class_name, &wcex))
+		{
+			wcex.cbSize = sizeof(WNDCLASSEX);
+			wcex.style = CS_HREDRAW | CS_VREDRAW;
+			wcex.lpfnWndProc = wnd_proc;
+			wcex.cbClsExtra = 0;
+			wcex.cbWndExtra = 0;
+			wcex.hInstance = instance;
+			wcex.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
+			wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+			wcex.hbrBackground = (HBRUSH)(COLOR_WINDOWTEXT + 1);
+			wcex.lpszMenuName = nullptr;
+			wcex.lpszClassName = class_name;
+			wcex.hIconSm = nullptr;
+			if (!RegisterClassExW(&wcex)) {
+				return nullptr;
+			}		
+		}
+		
+		auto const self = new Window(instance, device, comp, json);
+
+		std::string title("CEF OSR Mixer - ");
+		title.append(cef_version());
+		title.append(" - [gpu: ");
+		title.append(device->adapter_name());
+		title.append("]");		
+
+		auto const hwnd = CreateWindow(class_name,
+			to_utf16(title).c_str(),
+			WS_OVERLAPPEDWINDOW,
+			CW_USEDEFAULT, 0, CW_USEDEFAULT, 0,
+			nullptr,
+			nullptr,
+			instance,
+			(LPVOID)self);
+
+		if (IsWindow(hwnd))
+		{
+			// AdjustWindowRect can do something similar
+			RECT rc_outer, rc_inner;
+			GetWindowRect(hwnd, &rc_outer);
+			GetClientRect(hwnd, &rc_inner);
+
+			SetWindowPos(hwnd, nullptr, 0, 0,
+				width + ((rc_outer.right - rc_outer.left) - (rc_inner.right - rc_inner.left)),
+				height + ((rc_outer.bottom - rc_outer.top) - (rc_inner.bottom - rc_inner.top)),
+				SWP_NOMOVE | SWP_NOZORDER);
+
+			windows_.push_back(self);
+
+			// make the window visible now that we have D3D11 components ready
+			self->show();
+
+			return self;
+		}
+
+		return nullptr;
+	}
+
+	void show() {
+		ShowWindow(hwnd(), SW_SHOWNORMAL);
+	}
+
+	void tick(double t)
+	{
+		// update composition + layers based on time
+		composition_->tick(t);
+	}
+
+	void render()
+	{
+		auto ctx = device_->immedidate_context();
+		if (!ctx || !swapchain_) {
+			return;
+		}
+		
+		swapchain_->bind(ctx);
+
+		// is there a request to resize ... if so, resize
+		// both the swapchain and the composition
+		if (resize_)
+		{
+			RECT rc;
+			GetClientRect(hwnd(), &rc);
+			auto const width = rc.right - rc.left;
+			auto const height = rc.bottom - rc.top;
+			if (width && height)
+			{
+				resize_ = false;
+				composition_->resize(sync_interval_ != 0, width, height);
+				swapchain_->resize(width, height);
+			}
+		}
+
+		// clear the render-target
+		swapchain_->clear(0.0f, 0.0f, 1.0f, 1.0f);
+
+		// render our scene
+		composition_->render(ctx);
+
+		// present to window
+		swapchain_->present(sync_interval_);
+	}
+
+private:
+
+	static LRESULT CALLBACK wnd_proc(HWND hwnd, UINT message, WPARAM wp, LPARAM lp)
+	{
+		Window* self = reinterpret_cast<Window*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
+		if (!self)
+		{
+			if (message == WM_NCCREATE)
+			{
+				self = reinterpret_cast<Window*>(((LPCREATESTRUCT)lp)->lpCreateParams);
+				if (self) {
+					self->hwnd_ = hwnd;
+				}
+				SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)self);
+			}
+		}
+
+		LRESULT lr = 0L;
+		if (self)
+		{
+			if (message != WM_NCDESTROY)
+			{
+				if (self->wnd_proc_(message, wp, lp, lr)) {
+					return lr;
+				}
+			}
+			else
+			{
+				SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)(0));
+				delete self;
+			}		
+		}
+
+		lr = DefWindowProc(hwnd, message, wp, lp);
+		return lr;
+	}
+
+	bool wnd_proc_(UINT message, WPARAM wp, LPARAM lp, LRESULT result)
+	{
+		switch (message)
+		{
+			case WM_CREATE:
+				on_create();
+				break;
+
+			case WM_PAINT:
+			{
+				PAINTSTRUCT ps;
+				BeginPaint(hwnd(), &ps);
+				EndPaint(hwnd(), &ps);
+			}
+			break;
+
+			case WM_COMMAND:
+				switch (LOWORD(wp))
+				{
+					case ID_WINDOW_NEW:
+						on_new_window();
+						break;
+					case ID_WINDOW_VSYNC:
+						sync_interval_ = sync_interval_ ? 0 : 1;
+						resize_ = true;
+						break;
+					case ID_VIEW_DEVTOOLS:
+						//show_devtools_ = true;
+						break;
+					default: break;
+				}
+				break;
+
+			case WM_LBUTTONDOWN: 
+				on_mouse_click(MouseButton::Left, false, lp);
+				break;
+			case WM_LBUTTONUP: 
+				on_mouse_click(MouseButton::Left, true, lp);
+				break;
+			case WM_RBUTTONDOWN: 
+				on_mouse_click(MouseButton::Right, false, lp);
+				break;
+			case WM_RBUTTONUP: 
+				on_mouse_click(MouseButton::Right, true, lp);
+				break;
+
+			case WM_MOUSEMOVE: on_mouse_move(false, lp);
+				break;
+
+			case WM_SIZE:
+				// signal that we want a resize of output
+				resize_ = true;
+				break;
+
+			case WM_DESTROY:
+				PostQuitMessage(0);
+				break;
+
+			default: break;
+		}
+
+		return 0;
+	}
+
+	void on_create()
+	{
+		// create a D3D11 swapchain for the window
+		swapchain_ = device_->create_swapchain(hwnd());
+	}
+
+	void on_new_window()
+	{
+		RECT rc;
+		GetClientRect(hwnd(), &rc);
+		Window::open(instance_, json_, rc.right - rc.left, rc.bottom - rc.top);
+	}
+
+	//
+	// forward a Windows WM_XXX mouse Up/Down notification to the layers
+	//
+	void on_mouse_click(MouseButton button, bool up, LPARAM lp)
+	{
+		auto const x = ((int)(short)LOWORD(lp));
+		auto const y = ((int)(short)HIWORD(lp));
+		if (composition_) {
+			composition_->mouse_click(button, up, x, y);
+		}
+	}
+
+	//
+	// forward a Windows WM_XXX mouse move notification to the layers
+	//
+	void on_mouse_move(bool leave, LPARAM lp)
+	{
+		auto const x = ((int)(short)LOWORD(lp));
+		auto const y = ((int)(short)HIWORD(lp));
+		if (composition_) {
+			composition_->mouse_move(leave, x, y);
+		}
+	}
+
+};
+
 
 //
 // simple RIAA for CoInitialize/CoUninitialize
@@ -38,8 +333,6 @@ public:
 	}
 	~ComInitializer() { CoUninitialize(); }
 };
-
-std::shared_ptr<Composition> composition_;
 
 int APIENTRY wWinMain(HINSTANCE instance, HINSTANCE, LPWSTR, int)
 {
@@ -123,15 +416,7 @@ int APIENTRY wWinMain(HINSTANCE instance, HINSTANCE, LPWSTR, int)
 
 	// this demo uses WIC to load images .. so we need COM
 	ComInitializer com_init;
-
-	// create a D3D11 rendering device
-	auto device = d3d11::create_device();
-	if (!device) {
-		assert(0);
-		cef_uninitialize();
-		return 0;
-	}
-
+	
 	std::string json;
 
 	// if the url given on the command line is actually a local file ... 
@@ -212,46 +497,19 @@ int APIENTRY wWinMain(HINSTANCE instance, HINSTANCE, LPWSTR, int)
 		json = builder.str();
 	}
 
-	// create a composition to represent our 2D-scene
-	composition_ = create_composition(device, json);
-	if (!composition_) {
+	// create the first top-level window 
+	// (additional can be opened with Ctrl+W)
+	auto const window = Window::open(instance, json, width, height);
+	if (!window) 
+	{
 		assert(0);
 		cef_uninitialize();
 		return 0;
 	}
 	
-	std::string title("CEF OSR Mixer - ");
-	title.append(cef_version());
-
-	title.append(" - [gpu: ");
-	title.append(device->adapter_name());
-	title.append("]");
-
-	// create a window with our specific size
-	auto const window = create_window(
-			instance, title, composition_->width(), composition_->height());
-	if (!IsWindow(window)) {
-		assert(0);
-		cef_uninitialize();
-		return 0;
-	}
-
-	// create a D3D11 swapchain for the window
-	auto swapchain = device->create_swapchain(window);
-	if (!swapchain) {
-		assert(0);
-		cef_uninitialize();
-		return 0;
-	}
-
-	// make the window visible now that we have D3D11 components ready
-	ShowWindow(window, SW_NORMAL);
-
 	// load keyboard accelerators
 	HACCEL accel_table = 
 		LoadAccelerators(instance, MAKEINTRESOURCE(IDR_APPLICATION));
-
-	auto ctx = device->immedidate_context();
 
 	auto const start_time = time_now();
 
@@ -261,7 +519,7 @@ int APIENTRY wWinMain(HINSTANCE instance, HINSTANCE, LPWSTR, int)
 	{
 		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
 		{
-			if (!TranslateAccelerator(window, accel_table, &msg))
+			if (!TranslateAccelerator(msg.hwnd, accel_table, &msg))
 			{
 				TranslateMessage(&msg);
 				DispatchMessage(&msg);
@@ -269,161 +527,18 @@ int APIENTRY wWinMain(HINSTANCE instance, HINSTANCE, LPWSTR, int)
 		}
 		else
 		{
-			// update composition + layers based on time
 			auto const t = (time_now() - start_time) / 1000000.0;
-			composition_->tick(t);
-
-			swapchain->bind(ctx);
-
-			// is there a request to resize ... if so, resize
-			// both the swapchain and the composition
-			if (resize_) 
+			for (auto const& w : windows_) 
 			{
-				RECT rc;
-				GetClientRect(window, &rc);
-				width = rc.right - rc.left;
-				height = rc.bottom - rc.top;
-				if (width && height)
-				{
-					resize_ = false;
-					composition_->resize(sync_interval_ != 0, width, height);
-					swapchain->resize(width, height);
-				}
+				w->tick(t);
+				w->render();
 			}
-
-			// clear the render-target
-			swapchain->clear(0.0f, 0.0f, 1.0f, 1.0f);
-
-			// render our scene
-			composition_->render(ctx);
-
-			// present to window
-			swapchain->present(sync_interval_);
 		}
 	}
 
 	// force layers to be destroyed
-	composition_.reset();
+	//composition_.reset();
 
 	cef_uninitialize();
-	return 0;
-}
-
-HWND create_window(HINSTANCE instance, std::string const& title, int width, int height)
-{
-	LPCWSTR class_name = L"_main_window_";
-
-	WNDCLASSEXW wcex;
-	wcex.cbSize = sizeof(WNDCLASSEX);
-	wcex.style = CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc = wnd_proc;
-	wcex.cbClsExtra = 0;
-	wcex.cbWndExtra = 0;
-	wcex.hInstance = instance;
-	wcex.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
-	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
-	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOWTEXT + 1);
-	wcex.lpszMenuName = nullptr;
-	wcex.lpszClassName = class_name;
-	wcex.hIconSm = nullptr;
-	if (!RegisterClassExW(&wcex)) {
-		return nullptr;
-	}
-
-	auto const hwnd = CreateWindow(class_name,
-						to_utf16(title).c_str(),
-						WS_OVERLAPPEDWINDOW,
-						CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, 
-						nullptr, 
-						nullptr, 
-						instance, 
-						nullptr);
-
-	// AdjustWindowRect can do something similar
-	RECT rc_outer, rc_inner;
-	GetWindowRect(hwnd, &rc_outer);
-	GetClientRect(hwnd, &rc_inner);
-
-	SetWindowPos(hwnd, nullptr, 0, 0, 
-			width + ((rc_outer.right - rc_outer.left) - (rc_inner.right - rc_inner.left)),
-			height + ((rc_outer.bottom - rc_outer.top) - (rc_inner.bottom - rc_inner.top)),
-			SWP_NOMOVE | SWP_NOZORDER);
-
-	return hwnd;
-}
-
-//
-// forward a Windows WM_XXX mouse Up/Down notification to the layers
-//
-void on_mouse_click(MouseButton button, bool up, LPARAM lparam)
-{
-	auto const x = ((int)(short)LOWORD(lparam));
-	auto const y = ((int)(short)HIWORD(lparam));	
-	if (composition_) {
-		composition_->mouse_click(button, up, x, y);
-	}
-}
-
-//
-// forward a Windows WM_XXX mouse move notification to the layers
-//
-void on_mouse_move(bool leave, LPARAM lparam)
-{
-	auto const x = ((int)(short)LOWORD(lparam));
-	auto const y = ((int)(short)HIWORD(lparam));
-	if (composition_) {
-		composition_->mouse_move(leave, x, y);
-	}
-}
-
-LRESULT CALLBACK wnd_proc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
-{
-	switch (msg)
-	{
-		case WM_PAINT:
-			{
-				PAINTSTRUCT ps;
-				BeginPaint(hwnd, &ps);
-				EndPaint(hwnd, &ps);
-			}
-			break;
-
-		case WM_COMMAND:
-			switch (LOWORD(wparam))
-			{
-				case ID_WINDOW_VSYNC:
-					sync_interval_ = sync_interval_ ? 0 : 1;
-					resize_ = true;
-					break;
-				case ID_VIEW_DEVTOOLS:
-					show_devtools_ = true;
-					break;
-				default: break;
-			}
-			break;
-
-		case WM_LBUTTONDOWN: on_mouse_click(MouseButton::Left, false, lparam);
-			break;
-		case WM_LBUTTONUP: on_mouse_click(MouseButton::Left, true, lparam);
-			break;
-		case WM_RBUTTONDOWN: on_mouse_click(MouseButton::Right, false, lparam);
-			break;
-		case WM_RBUTTONUP: on_mouse_click(MouseButton::Right, true, lparam);
-			break;
-
-		case WM_MOUSEMOVE: on_mouse_move(false, lparam);
-			break;
-
-		case WM_SIZE: 
-			// signal that we want a resize of output
-			resize_ = true;
-			break;
-
-		case WM_DESTROY:
-			PostQuitMessage(0);
-			break;
-		default: 
-			return DefWindowProc(hwnd, msg, wparam, lparam);
-	}
 	return 0;
 }
